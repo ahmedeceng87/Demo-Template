2c79ab831611fbbddf7091eeb4b3440a0678cc16
ahmedeceng87 <107092820+ahmedeceng87@users.noreply.github.com>
2023-09-26 16:50:53 +0530

    Add files via upload

---
+++ Templates/templates/Demotemplate/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Templates
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"
+                                Copyright: NamePlaceHolder
+                                Title:     TitlePlaceHolder
+");
+        }
+    }
+}
